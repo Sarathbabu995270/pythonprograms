b2355a5dced087ee2a1253f4da207cf8fb853fba
Sarathbabu995270 <bsarath286@gmail.com>
2025-04-15 17:14:49 +0530

    make change

--- demo.py
+++ demo.py
@@ -52,6 +52,7 @@ b = 10
 print("Is a identical to b?", a is b)       # Identity (is)
 print("Is a not identical to b?", a is not b)  # Identity (is not)
 
+<<<<<<< HEAD
 #Hello world!
 
 
@@ -70,3 +71,12 @@ print("Is a not identical to b?", a is not b)  # Identity (is not)
 
 
 
+=======
+# Using a for loop
+for i in range(5):
+    print("Good Morning!")
+
+    # Using a for loop with numbering
+for i in range(1, 6):  # Start range from 1 to include numbering
+    print(f"{i}. Good Morning!")
+>>>>>>> 53dfb0c (make some changes)
