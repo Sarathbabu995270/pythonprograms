e26ff426977a0e39cd1fdb62670f133c0c94e745
Sarath995270 <bsarath286@gmailcom>
2025-04-26 08:07:45 +0530

    reverse string

--- method.py
+++ method.py
@@ -4,18 +4,10 @@
 for i in range(1, 6):
     print(f"{i}. Good Morning")
 
-# Program to reverse a number
-
 # Input from user
-number = int(input("Enter a number: "))
+text = input("Enter a string: ")
 
-# Initialize reversed number to 0
-reversed_number = 0
+# Reverse the string using slicing
+reversed_text = text[::-1]
 
-# Reverse the number
-while number != 0:
-    digit = number % 10  # Get the last digit
-    reversed_number = reversed_number * 10 + digit  # Append the digit to reversed number
-    number = number // 10  # Remove the last digit from the original number
-
-print(f"Reversed Number: {reversed_number}")
+print(f"Reversed String: {reversed_text}")
